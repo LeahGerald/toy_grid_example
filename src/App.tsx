ca6c717c390804d2316211e061e569ba1022a93e
leah <leah@trialspark.com>
2021-09-24 09:22:14 -0400

    basic grouping grid works!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { capitalize } from 'lodash'
 import './App.css';
+import Grouping from './groupingGrid';
+
+import DataGrid from 'react-data-grid';
+const url = "f_pilot_queries f_pilot_queries 2021-06-11T1536.csv"
+var request = new XMLHttpRequest();  
+request.open("GET", url, false);   
+request.send(null);  
+
+var csvData = new Array();
+// Retrived data from csv file content
+var jsonObject = request.responseText.split(/\r?\n|\r/);
+for (var i = 0; i < jsonObject.length; i++) {
+  csvData.push(jsonObject[i].split(','));
+}
+// set header index name
+csvData[0][0] = 'index'
+console.log(csvData);
+
+const columns = csvData[0].map((e: string) => ({key: e, name: capitalize(e)}))
+console.log(columns)
+const actualData = csvData.slice(1)
+const rows: any[] = []
+for (let i = 0; i < actualData.length; i++) {
+  const row = actualData[i]
+  let entry: any  = {}
+  for(let j = 0; j < row.length; j++) {
+    entry[columns[j].key] = row[j]
+  }
+  rows.push(entry)
+}
+console.log(columns)
+console.log(rows)
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Grouping />;
 }
 
 export default App;
